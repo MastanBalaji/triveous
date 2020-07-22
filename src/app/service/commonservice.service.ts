9928268898e007197b99e1115997d68e48330282
MastanBalaji <mastanbalaji@gmail.com>
2020-07-22 09:56:43 +0530

    changed http request

--- src/app/service/commonservice.service.ts
+++ src/app/service/commonservice.service.ts
@@ -8,6 +8,6 @@ export class CommonserviceService {
   constructor(private http:HttpClient) { }
 
   getdata(data,category){
-    return this.http.get('http://newsapi.org/v2/everything?q='+data+'&from=2020-06-22&sortBy='+category+'&apiKey=8a3f2ccb5c30442ca99d8e4853809b8a')
+    return this.http.get('https://newsapi.org/v2/everything?q='+data+'&from=2020-06-22&sortBy='+category+'&apiKey=8a3f2ccb5c30442ca99d8e4853809b8a')
   }
 }
